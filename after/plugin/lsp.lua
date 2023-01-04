2bf32627fcba9621316b8eb65e04703382c5d000
Nick Kadutskyi <nick@kadutskyi.com>
2023-01-04 12:14:01 -0800

    removes unnecessary requirements for lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,18 +3,9 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-  'tsserver',
-  'eslint',
   'sumneko_lua',
-  'rust_analyzer',
-  'html',
-  'cssls',
-  'phpactor',
-  'emmet_ls',
-  'intelephense',
   'vimls',
   'bashls',
-  'psalm',
   'jsonls',
   'lemminx',
   'yamlls'
