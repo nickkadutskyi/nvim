a972430cfdcfe78885c813421f9ceed62dc3560a
Nick Kadutskyi <nick@kadutskyi.com>
2023-06-07 13:45:29 -0700

    adds intelephense

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,22 +1,27 @@
-local lsp = require('lsp-zero')
+local lsp = require("lsp-zero")
 
-lsp.preset('recommended')
+lsp.preset("recommended")
 
 lsp.ensure_installed({
-  'vimls',
-  'bashls',
-  'jsonls',
-  'lemminx',
-  'yamlls'
+	"vimls",
+	"bashls",
+	"jsonls",
+	"lemminx",
+	"yamlls",
+	"intelephense",
 })
 
 lsp.set_preferences({
-  sign_icons = {
-    error = 'E',
-    warn = 'W',
-    hint = 'H',
-    info = 'I'
-  }
+	sign_icons = {
+		error = "E",
+		warn = "W",
+		hint = "H",
+		info = "I",
+	},
+})
+
+lsp.configure("intelephense", {
+	licenseKey = "00WS74ZNX2TKTI8",
 })
 
 lsp.nvim_workspace()
