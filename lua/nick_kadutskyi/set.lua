42a0afd6d5194db7cc6ecd9726e84f52bcff7c13
Nick Kadutskyi <nick@kadutskyi.com>
2023-12-10 18:00:11 -0800

    Fixes

--- lua/nick_kadutskyi/set.lua
+++ lua/nick_kadutskyi/set.lua
@@ -1,7 +1,8 @@
 vim.opt.cmdheight = 0
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv( "HOME" ) .. "/.vim/undodir"
+-- disabled for windows support
+-- vim.opt.undodir = os.getenv( "HOME" ) .. "/.vim/undodir"
 vim.opt.undofile = true
 
 
