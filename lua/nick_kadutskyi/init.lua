3e3fc648cd1ed85f74d3e5178898e367b1e3f571
Nick Kadutskyi <nick@kadutskyi.com>
2024-02-10 16:16:02 -0800

    fixes current dir selection

--- lua/nick_kadutskyi/init.lua
+++ lua/nick_kadutskyi/init.lua
@@ -1,9 +1,9 @@
 require("nick_kadutskyi.set")
 require("nick_kadutskyi.remap")
 
--- if opened a dir set it as current dir to help narrow down telescope's scope
-if vim.fn.isdirectory(vim.fn.expand('%')) == 1 then
-  vim.api.nvim_set_current_dir(vim.fn.expand('%'))
-else
-  vim.api.nvim_set_current_dir(vim.fn.expand('%:p:h'))
+-- if opened a dir set it as current dir to help narrow down fzf scope
+if vim.fn.isdirectory(vim.fn.expand("%")) == 1 then
+	vim.api.nvim_set_current_dir(vim.fn.expand("%"))
+elseif vim.fn.filereadable(vim.fn.expand("%")) == 1 then
+	vim.api.nvim_set_current_dir(vim.fn.expand("%:p:h"))
 end
