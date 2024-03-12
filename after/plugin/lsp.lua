7c6c1abd6da36f07bbb526a6b2f60d600e8dc70c
Nick Kadutskyi <nick@kadutskyi.com>
2024-03-12 10:01:14 -0700

    disables semantic tokens temporarily

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,35 +5,38 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.set_preferences({
-	sign_icons = {
-		error = "E",
-		warn = "W",
-		hint = "H",
-		info = "I",
-	},
+  sign_icons = {
+    error = "E",
+    warn = "W",
+    hint = "H",
+    info = "I",
+  },
 })
 
 -- lsp.nvim_workspace()
 
 -- Needed for nvim-navic
 lsp.on_attach(function(client, bufnr)
-	lsp.default_keymaps({ buffer = bufnr })
+  lsp.default_keymaps({ buffer = bufnr })
 
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
+  -- Disabled semantic tokens before I deal with highlights
+  client.server_capabilities.semanticTokensProvider = nil
+
+  if client.server_capabilities.documentSymbolProvider then
+    require("nvim-navic").attach(client, bufnr)
+  end
 end)
 
 -- LSP Formatters
 lsp.format_mapping("gq", {
-	format_opts = {
-		async = true,
-		timeout_ms = 10000,
-	},
-	servers = {
-		["prettierd"] = { "javascript", "typescript" },
-		["rust_analyzer"] = { "rust" },
-	},
+  format_opts = {
+    async = true,
+    timeout_ms = 10000,
+  },
+  servers     = {
+    ["prettierd"] = { "javascript", "typescript" },
+    ["rust_analyzer"] = { "rust" },
+  },
 })
 
 lsp.setup()
