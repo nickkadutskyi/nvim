2fa46cfcd923760a0a6d1a0ae0895f7ccb5962b0
Nick Kadutskyi <nick@kadutskyi.com>
2024-03-14 16:15:11 -0700

    adds highlights on yanking

--- lua/nick_kadutskyi/init.lua
+++ lua/nick_kadutskyi/init.lua
@@ -93,3 +93,17 @@ nnoremap("fl", function()
     return "fl"
   end
 end, { noremap = false, expr = true })
+
+
+-- FEATUERS
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
