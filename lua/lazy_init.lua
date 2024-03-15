a30d31d2c0ffb76952bc0392a10373aa21fe758d
Nick Kadutskyi <nick@kadutskyi.com>
2024-03-15 10:26:42 -0700

    adjust plugin configs

--- lua/lazy_init.lua
+++ lua/lazy_init.lua
@@ -15,31 +15,12 @@ vim.opt.rtp:prepend(lazypath)
 -- Initializing lazy.nvim
 require("lazy").setup(
   {
-    'tpope/vim-sleuth',   -- Detect tabstop and shiftwidth automatically
-    "github/copilot.vim", -- AI Assistant
-    {                     -- Useful plugin to show you pending keybinds.
-      'folke/which-key.nvim',
-      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-      config = function() -- This is the function that runs, AFTER loading
-        require('which-key').setup()
-
-        -- Document existing key chains
-        require('which-key').register {
-          ['<leader>c'] = { name = '[C]hange & keep register', _ = 'which_key_ignore' },
-          ['<leader>C'] = { name = '[C]hange line & keep register', _ = 'which_key_ignore' },
-          ['<leader>d'] = { name = '[D]elete & keep register', _ = 'which_key_ignore' },
-          ['<leader>D'] = { name = '[D]elete line & keep register', _ = 'which_key_ignore' },
-          ['<leader>x'] = { name = '[D]elete char & keep register', _ = 'which_key_ignore' },
-          ['<leader>p'] = { name = '[P]aste over selection & keep regiser', _ = 'which_key_ignore' },
-
-          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        }
-      end,
-    },
-    -- Disables treesitter if the file has 100000 cols wide lines
-    {
+    'tpope/vim-sleuth',          -- Detect tabstop and shiftwidth automatically
+    "github/copilot.vim",        -- AI Assistant
+    "xiyaowong/virtcolumn.nvim", -- Visual guides
+    "lewis6991/gitsigns.nvim",   -- Visibility for changes comparde to current git branch in the gutter
+    "tpope/vim-fugitive",        -- For git diff
+    {                            -- Disables treesitter if the file has 100000 cols wide lines
       "LunarVim/bigfile.nvim",
       lazy = false,
       event = { "FileReadPre", "BufReadPre", "User FileOpened" },
@@ -317,8 +298,6 @@ require("lazy").setup(
         })
       end
     },
-    "lewis6991/gitsigns.nvim", -- Visibility for changes comparde to current git branch in the gutter
-    "tpope/vim-fugitive",      -- For git diff
     -- Scrollbar to also show git changes not visible in current view
     {
       "petertriho/nvim-scrollbar",
@@ -365,10 +344,6 @@ require("lazy").setup(
         -- add any options here
       },
       lazy = false,
-    },
-    -- Visual guides
-    {
-      "xiyaowong/virtcolumn.nvim",
     },
     {
       "folke/trouble.nvim",
@@ -422,9 +397,30 @@ require("lazy").setup(
         },
       },
     },
+    -- UNNECESSARY
+    {                     -- Useful plugin to show you pending keybinds.
+      'folke/which-key.nvim',
+      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+      config = function() -- This is the function that runs, AFTER loading
+        require('which-key').setup()
+
+        -- Document existing key chains
+        require('which-key').register {
+          ['<leader>c'] = { name = '[C]hange & keep register', _ = 'which_key_ignore' },
+          ['<leader>C'] = { name = '[C]hange line & keep register', _ = 'which_key_ignore' },
+          ['<leader>d'] = { name = '[D]elete & keep register', _ = 'which_key_ignore' },
+          ['<leader>D'] = { name = '[D]elete line & keep register', _ = 'which_key_ignore' },
+          ['<leader>x'] = { name = '[D]elete char & keep register', _ = 'which_key_ignore' },
+          ['<leader>p'] = { name = '[P]aste over selection & keep regiser', _ = 'which_key_ignore' },
+
+          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        }
+      end,
+    },
   },
-  -- Configs
-  {
+  { -- Configs
     ui = {
       border = "rounded",
       title = { { " Plugin Manager ", "JBFloatBorder" } },
