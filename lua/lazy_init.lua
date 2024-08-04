03a06cf8f6bd8be73b83a79ce48331152788c404
Nick Kadutskyi <nick@kadutskyi.com>
2024-08-03 22:30:14 -0700

    Updates plugins

--- lua/lazy_init.lua
+++ lua/lazy_init.lua
@@ -216,7 +216,7 @@ require("lazy").setup(
           end
         end)
 
-        -- Configure nil_ls
+        -- Configure nil_ls for nix
         local caps = vim.tbl_deep_extend(
           'force',
           vim.lsp.protocol.make_client_capabilities(),
@@ -337,6 +337,7 @@ require("lazy").setup(
             -- Use a sub-list to run only the first available formatter
             javascript = { { "prettierd", "prettier" } },
             css = { { "prettierd", "prettier" } },
+            php = {"php_cs_fixer"}
           },
         })
       end
@@ -494,27 +495,27 @@ require("lazy").setup(
       },
     },
     -- UNNECESSARY
-    {                     -- Useful plugin to show you pending keybinds.
-      'folke/which-key.nvim',
-      event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-      config = function() -- This is the function that runs, AFTER loading
-        require('which-key').setup()
-
-        -- Document existing key chains
-        require('which-key').register {
-          ['<leader>c'] = { name = '[C]hange & keep register', _ = 'which_key_ignore' },
-          ['<leader>C'] = { name = '[C]hange line & keep register', _ = 'which_key_ignore' },
-          ['<leader>d'] = { name = '[D]elete & keep register', _ = 'which_key_ignore' },
-          ['<leader>D'] = { name = '[D]elete line & keep register', _ = 'which_key_ignore' },
-          ['<leader>x'] = { name = '[D]elete char & keep register', _ = 'which_key_ignore' },
-          ['<leader>p'] = { name = '[P]aste over selection & keep regiser', _ = 'which_key_ignore' },
-
-          ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-          ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-          ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        }
-      end,
-    },
+    -- {                     -- Useful plugin to show you pending keybinds.
+    --   'folke/which-key.nvim',
+    --   event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+    --   config = function() -- This is the function that runs, AFTER loading
+    --     require('which-key').setup()
+    --
+    --     -- Document existing key chains
+    --     -- require('which-key').register {
+    --       -- ['<leader>c'] = { name = '[C]hange & keep register', _ = 'which_key_ignore' },
+    --       -- ['<leader>C'] = { name = '[C]hange line & keep register', _ = 'which_key_ignore' },
+    --       -- ['<leader>d'] = { name = '[D]elete & keep register', _ = 'which_key_ignore' },
+    --       -- ['<leader>D'] = { name = '[D]elete line & keep register', _ = 'which_key_ignore' },
+    --       -- ['<leader>x'] = { name = '[D]elete char & keep register', _ = 'which_key_ignore' },
+    --       -- ['<leader>p'] = { name = '[P]aste over selection & keep regiser', _ = 'which_key_ignore' },
+    --       --
+    --       -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+    --       -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
+    --       -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+    --     -- }
+    --   end,
+    -- },
   },
   { -- Configs
     ui = {
