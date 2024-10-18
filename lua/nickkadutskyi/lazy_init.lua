2e6be60dd9b770620a00f95fe5e13810b639afea
Nick Kadutskyi <nick@kadutskyi.com>
2024-10-18 16:13:11 -0700

    fixes and restructuring

--- lua/nickkadutskyi/lazy_init.lua
+++ lua/nickkadutskyi/lazy_init.lua
@@ -20,24 +20,6 @@ require("lazy").setup({
     spec = {
         { import = "nickkadutskyi.plugins" },
         {
-            "nvim-tree/nvim-web-devicons", -- For getting pretty icons, but requires a Nerd Font.
-            config = function()
-                require("nvim-web-devicons").setup({
-                    override = { -- your personnal icons can go here (to override)
-                        zsh = { icon = "", color = "#428850", cterm_color = "65", name = "Zsh" },
-                    },
-                    default = true, -- globally enable default icons (default to false)
-                    strict = true, -- globally enable "strict" selection of icons (default to false)
-                    override_by_filename = { -- same as `override` but for overrides by filename (requires `strict` to be true)
-                        [".gitignore"] = { icon = "", color = "#f1502f", name = "Gitignore" },
-                    },
-                    override_by_extension = { -- same as `override` but for overrides by extension (requires `strict` to be true)
-                        ["log"] = { icon = "", color = "#81e043", name = "Log" },
-                    },
-                })
-            end,
-        },
-        {
             "rachartier/tiny-inline-diagnostic.nvim", -- better diagnostics
             event = "VeryLazy", -- Or `LspAttach`
             config = function()
