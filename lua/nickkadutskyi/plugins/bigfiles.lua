b66d0351b3c05f84ec42ea906ea7a90ea041e275
Nick Kadutskyi <nick@kadutskyi.com>
2024-10-19 08:52:47 -0700

    cleans and fixes bigfile conflict with fugitiv

--- lua/nickkadutskyi/plugins/bigfiles.lua
+++ lua/nickkadutskyi/plugins/bigfiles.lua
@@ -13,8 +13,19 @@ return {
         filesize = 3,
     },
     config = function(plugin, opts)
+        local uv = vim.uv or vim.loop
         require("bigfile").setup({
             pattern = function(bufnr, filesize_mib)
+                local filepath = vim.api.nvim_buf_get_name(bufnr)
+                -- Check if the filepath is a regular file
+                local stat = uv.fs_stat(filepath)
+                if not stat or stat.type ~= "file" then
+                    -- If it's not a file, skip the bigfile checks
+                    return false
+                end
+                -- Convert bytes to MiB
+                local filesize_mib_precise = stat.size / (1024 * 1024)
+
                 local file_content = vim.fn.readfile(vim.api.nvim_buf_get_name(bufnr))
                 -- local file_len = #file_content
                 local filetype = vim.filetype.match({ buf = bufnr })
@@ -24,7 +35,7 @@ return {
                     #opts.features > 1 and "are" or "is"
                 )
                 -- Check filesize
-                local due_to = filesize_mib <= opts.filesize and ""
+                local due_to = filesize_mib_precise <= opts.filesize and ""
                     or string.format(" file size being greater than %sMiB (%sMiB).", opts.filesize, filesize_mib)
                 -- Check length of lines
                 if due_to == "" then
