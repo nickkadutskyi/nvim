32befd85e6c5c8dbb61baffc0e6ed4ff5b6321fc
Nick Kadutskyi <nick@kadutskyi.com>
2024-10-22 10:22:07 -0700

    improves formating for autocoplete items to show source

--- lua/nickkadutskyi/plugins/autocomplete.lua
+++ lua/nickkadutskyi/plugins/autocomplete.lua
@@ -32,6 +32,29 @@ return {
                 completion = {
                     completeopt = "menu,menuone,noinsert",
                 },
+                formatting = {
+                    expandable_indicator = true,
+                    fields = { "abbr", "kind", "menu" },
+                    format = function(entry, item)
+                        local n = entry.source.name
+                        local label = ""
+
+                        if n == "nvim_lsp" then
+                            label = "[LSP]"
+                        elseif n == "nvim_lua" then
+                            label = "[nvim]"
+                        else
+                            label = string.format("[%s]", n)
+                        end
+
+                        if item.menu ~= nil then
+                            item.menu = string.format("%s %s", label, item.menu)
+                        else
+                            item.menu = label
+                        end
+                        return item
+                    end,
+                },
                 window = {
                     completion = cmp.config.window.bordered(),
                     documentation = cmp.config.window.bordered(),
