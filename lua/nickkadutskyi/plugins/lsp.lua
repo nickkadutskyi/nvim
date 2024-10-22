3ebe9c27562ca31a900eaffe4fda2ff50ad706ce
Nick Kadutskyi <nick@kadutskyi.com>
2024-10-22 10:22:07 -0700

    improves formating for autocoplete items to show source

--- lua/nickkadutskyi/plugins/lsp.lua
+++ lua/nickkadutskyi/plugins/lsp.lua
@@ -24,7 +24,7 @@ return {
                 },
             })
 
-            -- Mason-lspconfig—before require("lspconfig")[server_name].setup
+            -- Mason-lspconfig—before manual require("lspconfig")[server_name].setup
             local cmp_lsp = require("cmp_nvim_lsp")
             local capabilities = vim.tbl_deep_extend(
                 "force",
