c6a931e9eb2fa6db0bb93d32c86ec34bf76cd431
Nick Kadutskyi <nick@kadutskyi.com>
2024-10-24 17:03:23 -0700

    adds auto install for lsp servers via nix if it's not installed and missing in mason

--- lua/nickkadutskyi/plugins/lsp.lua
+++ lua/nickkadutskyi/plugins/lsp.lua
@@ -2,11 +2,7 @@ return {
     {
         -- For installing langauge servers, formatters, linters, DAPs
         "williamboman/mason.nvim",
-        opts = {
-            ui = {
-                border = "rounded",
-            },
-        },
+        opts = { ui = { border = "rounded" } },
     },
     {
         -- Uses LSP to show current code context—used in status line
@@ -23,6 +19,9 @@ return {
             "hrsh7th/cmp-nvim-lsp",
         },
         config = function(plugin, _)
+            -- FIXME is this a proper api?
+            require("lspconfig.ui.windows").default_options.border = "rounded"
+
             local ensure_ls = {
                 "lua_ls",
                 "ts_ls",
@@ -31,12 +30,16 @@ return {
                 "nixd",
             }
 
-            -- language servers to handle with mason
+            -- language servers to install and configure with mason
             local mason_ls = {}
-            -- language servers to configure with lspconfig
+            -- language servers existing in the system to configure with lspconfig
             local lspconfig_ls = {}
+            -- language servers non existing in the system but to try to run via nix run
+            local nix_ls = {}
 
             local mason_dir = require("mason.settings").current.install_root_dir
+            local nix_path = vim.fn.exepath("nix")
+            -- categorize language servers by how they are configured
             for _, server_name in ipairs(ensure_ls) do
                 local success, config = pcall(require, "lspconfig.configs." .. server_name)
                 if success then
@@ -44,10 +47,24 @@ return {
                     if cmd and type(cmd) == "table" and not vim.tbl_isempty(cmd) then
                         local path = vim.fn.exepath(cmd[1])
                         -- missing from the system or already installed with mason
-                        if #path == 0 or string.find(path, mason_dir) ~= nil then
-                            table.insert(mason_ls, server_name)
+                        if
+                            (#path == 0 or string.find(path, mason_dir) ~= nil)
+                            and require("mason-lspconfig").get_mappings().mason_to_lspconfig[server_name] ~= nil
+                        then
+                            mason_ls[server_name] = config.default_config
+                        elseif #path ~= 0 then
+                            lspconfig_ls[server_name] = config.default_config
+                        elseif #nix_path ~= 0 then
+                            nix_ls[server_name] = config.default_config
                         else
-                            table.insert(lspconfig_ls, server_name)
+                            vim.notify(
+                                string.format(
+                                    "Packag %s is not provided by Mason and nix is not installead. Try to install it manually.",
+                                    server_name
+                                ),
+                                vim.log.levels.WARN,
+                                { title = "Plugin " .. plugin.name .. " config()" }
+                            )
                         end
                     end
                 else
@@ -62,6 +79,7 @@ return {
                 end
             end
 
+            -- capabilities to provide to lspconfig
             local cmp_lsp = require("cmp_nvim_lsp")
             local capabilities = vim.tbl_deep_extend(
                 "force",
@@ -70,12 +88,28 @@ return {
                 cmp_lsp.default_capabilities()
             )
 
+            local runtime_path = vim.split(package.path, ";")
+            table.insert(runtime_path, "lua/?.lua")
+            table.insert(runtime_path, "lua/?/init.lua")
             local ls_configs = {
                 ["lua_ls"] = {
                     settings = {
                         Lua = {
+                            -- Disable telemetry
+                            telemetry = { enable = false },
+                            runtime = {
+                                version = "LuaJIT",
+                                path = runtime_path,
+                            },
                             diagnostics = {
                                 globals = { "vim" },
+                            },
+                            workspace = {
+                                checkThirdParty = false,
+                                library = {
+                                    vim.env.VIMRUNTIME,
+                                    "${3rd}/luv/library",
+                                },
                             },
                         },
                     },
@@ -92,6 +126,7 @@ return {
                     },
                 },
                 ["nixd"] = {
+                    random = "",
                     settings = {
                         ["nixd"] = {
                             formatting = {
@@ -127,13 +162,43 @@ return {
 
             -- install language servers with mason and configure
             require("mason-lspconfig").setup({
-                ensure_installed = mason_ls,
+                ensure_installed = vim.tbl_keys(mason_ls),
                 handlers = { setup_ls },
             })
 
             -- configure language servers with lspconfig
-            for _, server_name in ipairs(lspconfig_ls) do
+            for server_name, config in pairs(lspconfig_ls) do
                 setup_ls(server_name)
+            end
+
+            -- configure language servers with lspconfig to run via `nix run`
+            for server_name, config in pairs(nix_ls) do
+                local cmd = config.cmd[1]
+                -- confirm store path
+                vim.system({ "nix", "path-info", "--json", "nixpkgs#" .. cmd }, { text = true }, function(o)
+                    if o.code == 0 then
+                        vim.schedule(function()
+                            -- configure languge server with lspconfig
+                            table.remove(config.cmd, 1)
+                            local nix_cmd = vim.list_extend({ "nix", "run", "nixpkgs#" .. cmd, "--" }, config.cmd)
+                            setup_ls(server_name, { cmd = nix_cmd })
+
+                            -- try to start language server if filetype matches in case server was configured after opening a file
+                            for _, ext in ipairs(config.filetypes) do
+                                if string.match(vim.api.nvim_buf_get_name(0), "%." .. ext .. "$") ~= nil then
+                                    vim.cmd("LspStart " .. server_name)
+                                    break
+                                end
+                            end
+                        end)
+                    else
+                        vim.notify(
+                            string.format("Failed to get path-info fro %s nix package due: %s", cmd, o.stderr),
+                            vim.log.levels.ERROR,
+                            { title = "Plugin " .. plugin.name .. " config()" }
+                        )
+                    end
+                end)
             end
 
             -- Conifgures LspAttach (on_attach) event for all language servers
