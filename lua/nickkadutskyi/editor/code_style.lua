65384ca7d3ae93f7452fd124bd1e6cde554ee1e0
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-19 10:56:17 -0800

    Moves code style configs to .editorconfig

--- lua/nickkadutskyi/editor/code_style.lua
+++ lua/nickkadutskyi/editor/code_style.lua
@@ -1,6 +1,6 @@
 -- Soft wrap
 vim.opt.wrap = false
--- Soft wrap at linebreak
+-- Soft wrap at linebreak - disabled for now
 vim.opt.linebreak = false
 -- Better indentation for wrapped lines
 if vim.fn.has("linebreak") == 1 then
@@ -8,27 +8,29 @@ if vim.fn.has("linebreak") == 1 then
     vim.opt.showbreak = "↳ "
     vim.opt.breakindentopt = { shift = 0, min = 20, sbr = true }
 end
--- Hard wrap -- Better handle this via formatters and .editorconfig rules
--- vim.opt.textwidth = 120
--- Set the tab size to 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
--- Use spaces instead of tabs
-vim.opt.expandtab = true
+
+-- Handled via .editorconfig
+-- vim.opt.textwidth = 120 -- max_line_length
+-- vim.opt.tabstop = 2 -- tab_width
+-- vim.opt.expandtab = true -- indent_style
+-- vim.opt.fileencoding = "utf-8" -- charset
+-- vim.opt.softtabstop = 2 -- indent_size
+-- vim.opt.shiftwidth = 2 -- indent_size
+
 -- Indents next line if current is indented
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 -- Adds visual guides
 -- vim.opt.colorcolumn = "80,100,120" -- defined in plugin
 -- Allow to move to one column past the end of the line
-vim.opt.virtualedit = 'onemore'
+vim.opt.virtualedit = "onemore"
 
 -- Lazy.nvim module
 return {
     {
         -- Initializes conform.nvim fro code formatting
         "stevearc/conform.nvim",
+        -- Merged with nickkadutskyi.lanaguages_frameworks
         opts = {
             default_format_opts = {
                 lsp_format = "fallback",
@@ -44,9 +46,5 @@ return {
                 desc = "Code: [r]eformat [c]ode",
             })
         end,
-    },
-    {
-        -- Adjusts tab size automatically
-        "tpope/vim-sleuth",
     },
 }
