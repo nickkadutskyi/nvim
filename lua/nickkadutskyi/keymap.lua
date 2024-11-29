33cd9211510b406515af4c73030d64ae3e5d5852
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-29 14:25:04 -0800

    Allow more ways to navigate between panes

--- lua/nickkadutskyi/keymap.lua
+++ lua/nickkadutskyi/keymap.lua
@@ -75,10 +75,14 @@ vim.keymap.set(
 
 -- Keybinds to make split navigation easier.
 -- Use CTRL+<hjkl> to switch between windows
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set({ "n", "i" }, "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set({ "n", "i" }, "<C-Left>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set({ "n", "i" }, "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set({ "n", "i" }, "<C-Right>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set({ "n", "i" }, "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set({ "n", "i" }, "<C-Down>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set({ "n", "i" }, "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set({ "n", "i" }, "<C-Up>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- Lazy.nvim modules
 return {
