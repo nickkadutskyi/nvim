63b362cb411eeccbeb273a1dd7b302addd0e3617
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-28 17:14:59 -0800

    Improves Lua highlighting

--- lua/nickkadutskyi/languages_frameworks/lua.lua
+++ lua/nickkadutskyi/languages_frameworks/lua.lua
@@ -4,6 +4,8 @@ return {
         opts = function(_, opts)
             vim.list_extend(opts.ensure_installed, {
                 "lua",
+                "luadoc",
+                "luap", -- for regex patterns (Lua Patterns)
             })
         end,
     },
