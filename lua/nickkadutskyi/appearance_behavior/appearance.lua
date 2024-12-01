1da3b42d7c8980fcea50d2458a741641ecc91f9e
Nick Kadutskyi <nick@kadutskyi.com>
2024-11-30 20:55:26 -0800

    Fixes

--- lua/nickkadutskyi/appearance_behavior/appearance.lua
+++ lua/nickkadutskyi/appearance_behavior/appearance.lua
@@ -58,10 +58,20 @@ if vim.env.TMUX then
     })
 end
 
+local function macos_background()
+    if vim.fn.has("macunix") == 1 then -- Sets default jb_style based on MacOs theme
+        if io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null"):read() == "Dark" then
+            vim.o.background = "dark"
+        else
+            vim.o.background = "light"
+        end
+    end
+end
 local function set_background(callback)
     -- looks for env variables to fore a specific background
     local theme = os.getenv("theme") or os.getenv("colorscheme_bg") or os.getenv("neovim_bg")
     if theme == nil then
+        macos_background()
         return type(callback) == "function" and callback()
     else
         if theme == "light" or theme == "l" then
@@ -69,13 +79,7 @@ local function set_background(callback)
         elseif theme == "dark" or theme == "d" then
             vim.o.background = "dark"
         else
-            if vim.fn.has("macunix") == 1 then -- Sets default jb_style based on MacOs theme
-                if io.popen("defaults read -g AppleInterfaceStyle 2>/dev/null"):read() == "Dark" then
-                    vim.o.background = "dark"
-                else
-                    vim.o.background = "light"
-                end
-            end
+            macos_background()
             vim.notify(
                 string.format(
                     "Invalid theme: '%s'.\nKeeping current: '%s'.\nExpected: 'light', 'l', 'dark' or 'd'.",
@@ -94,9 +98,7 @@ end
 vim.opt.mouse = "a"
 -- Check if Neovim is running RPC server
 local servername = vim.fn.has("nvim-0.9") and vim.api.nvim_get_vvar("servername") or ""
-if servername ~= "" then
-    set_background()
-end
+set_background()
 
 ---@type LazySpec
 return {
