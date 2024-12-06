39cf5bd10183770b51fceb1f053e1ee7c0be1af3
Nick Kadutskyi <nick@kadutskyi.com>
2024-12-06 05:29:51 -0800

    fixes

--- lua/nickkadutskyi/other/search.lua
+++ lua/nickkadutskyi/other/search.lua
@@ -52,40 +52,6 @@ return {
                         end,
                     },
                 },
-                keymap = {
-                    builtin = {
-                        ["<Esc>"] = "hide",
-                        ["<F1>"] = "toggle-help",
-                        ["<c-n>"] = "toggle-fullscreen",
-                        -- Only valid with the 'builtin' previewer
-                        ["<F3>"] = "toggle-preview-wrap",
-                        ["<c-p>"] = "toggle-preview",
-                        ["<F5>"] = "toggle-preview-ccw",
-                        ["<F6>"] = "toggle-preview-cw",
-                        ["<C-d>"] = "preview-page-down",
-                        ["<C-u>"] = "preview-page-up",
-                        ["<S-left>"] = "preview-page-reset",
-                    },
-                    fzf = {
-                        ["ctrl-z"] = "abort",
-                        ["ctrl-f"] = "forward-char",
-                        ["ctrl-b"] = "backward-char",
-                        ["ctrl-a"] = "beginning-of-line",
-                        ["ctrl-e"] = "end-of-line",
-                        ["alt-a"] = "toggle-all",
-                        -- Only valid with fzf previewers (bat/cat/git/etc)
-                        ["f3"] = "toggle-preview-wrap",
-                        ["f4"] = "toggle-preview",
-                        ["ctrl-d"] = "preview-page-down",
-                        ["ctrl-u"] = "preview-page-up",
-                    },
-                },
-                -- keymap = {
-                --     builtin = {
-                --         true, -- inherit all other default binds
-                --         ["<Esc>"] = "hide",
-                --     },
-                -- },
                 fzf_colors = true,
                 fzf_opts = {
                     ["--layout"] = "reverse",
@@ -103,12 +69,6 @@ return {
                     prompt = "  ",
                 },
                 files = {
-                    -- keymap = {
-                    --     builtin = {
-                    --         -- true, -- inherit all other default binds
-                    --         ["<Esc>"] = "hide",
-                    --     },
-                    -- },
                     winopts = {
                         title = " Files ",
                         title_pos = "center",
