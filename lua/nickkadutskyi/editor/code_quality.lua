3b4f49d1d95910c15f212073e1d316d81f1b9bbe
Nick Kadutskyi <nick@kadutskyi.com>
2025-01-05 17:23:26 -0800

    Fixes

--- lua/nickkadutskyi/editor/code_quality.lua
+++ lua/nickkadutskyi/editor/code_quality.lua
@@ -24,10 +24,12 @@ return {
                 local timer = vim.uv.new_timer()
                 return function(...)
                     local argv = { ... }
-                    timer:start(ms, 0, function()
-                        timer:stop()
-                        vim.schedule_wrap(fn)(unpack(argv))
-                    end)
+                    local r = timer ~= nil
+                            and timer:start(ms, 0, function()
+                                timer:stop()
+                                vim.schedule_wrap(fn)(unpack(argv))
+                            end)
+                        or nil
                 end
             end
 
