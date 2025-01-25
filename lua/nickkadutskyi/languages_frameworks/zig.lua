5b2afcd9b7f8f026361e32f364a7c7bbf0e14cdc
Nick Kadutskyi <nick@kadutskyi.com>
2025-01-25 15:44:27 -0800

    Adds zig support

---
+++ lua/nickkadutskyi/languages_frameworks/zig.lua
@@ -0,0 +1,35 @@
+---@type LazySpec
+return {
+    { -- Color Scheme
+        "nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "zig",
+            })
+        end,
+    },
+    { -- Code Style
+        "conform.nvim",
+        opts = {
+            formatters_by_ft = {
+                zig = { "zigfmt", lsp_format = "fallback" },
+            },
+        },
+    },
+    { -- Quality Tools (moved to LSP)
+        "nvim-lint",
+        opts = {
+            linters_by_ft = {
+                zig = { "zig" },
+            },
+        },
+    },
+    { -- Language Servers
+        "nvim-lspconfig",
+        opts = {
+            servers = {
+                zls = {},
+            },
+        },
+    },
+}
