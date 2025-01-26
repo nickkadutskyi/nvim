3083ba432669babd7789a5b1c7ef4d3acc567fc0
Nick Kadutskyi <nick@kadutskyi.com>
2025-01-25 16:04:03 -0800

    Adds java support

---
+++ lua/nickkadutskyi/languages_frameworks/java.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+return {
+    { -- Color Scheme
+        "nvim-treesitter",
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed, {
+                "java",
+            })
+        end,
+    },
+    { -- Language Servers
+        "nvim-lspconfig",
+        opts = {
+            servers = {
+                java_language_server = {},
+            },
+        },
+    },
+}
