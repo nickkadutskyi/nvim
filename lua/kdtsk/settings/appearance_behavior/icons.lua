5e6de1998d2aae6aca023b1782ff79706b108679
Nick Kadutskyi <nick@kadutskyi.com>
2025-05-18 00:20:15 -0700

    style(icons): reformat

--- lua/kdtsk/settings/appearance_behavior/icons.lua
+++ lua/kdtsk/settings/appearance_behavior/icons.lua
@@ -1,19 +1,39 @@
+---@type LazySpec
 return {
-    {
-        -- Gutter or statusline icons, requires a Nerd Font.
+    { -- Gutter or statusline icons, requires a Nerd Font.
         "nvim-tree/nvim-web-devicons",
         config = function()
             require("nvim-web-devicons").setup({
-                override = { -- your personnal icons can go here (to override)
-                    zsh = { icon = "", color = "#428850", cterm_color = "65", name = "Zsh" },
+                override = { -- your personal icons can go here (to override)
+                    zsh = {
+                        icon = "",
+                        color = "#428850",
+                        cterm_color = "65",
+                        name = "Zsh",
+                    },
                 },
-                default = true, -- globally enable default icons (default to false)
-                strict = true, -- globally enable "strict" selection of icons (default to false)
-                override_by_filename = { -- same as `override` but for overrides by filename (requires `strict` to be true)
-                    [".gitignore"] = { icon = "", color = "#f1502f", name = "Gitignore" },
+                -- globally enable default icons (default to false)
+                default = true,
+                -- globally enable "strict" selection of icons
+                -- (default to false)
+                strict = true,
+                -- same as `override` but for overrides by filename
+                -- (requires `strict` to be true)
+                override_by_filename = {
+                    [".gitignore"] = {
+                        icon = "",
+                        color = "#f1502f",
+                        name = "Gitignore",
+                    },
                 },
-                override_by_extension = { -- same as `override` but for overrides by extension (requires `strict` to be true)
-                    ["log"] = { icon = "", color = "#81e043", name = "Log" },
+                -- same as `override` but for overrides by extension
+                -- (requires `strict` to be true)
+                override_by_extension = {
+                    ["log"] = {
+                        icon = "",
+                        color = "#81e043",
+                        name = "Log",
+                    },
                 },
             })
         end,
