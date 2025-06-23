56eaefe6a13b45925e51911270cf73d1bc557f8b
Nick Kadutskyi <nick@kadutskyi.com>
2025-06-22 17:57:20 -0700

    adjusts git status in statusbar

--- lua/kdtsk/settings/languages_frameworks/markdown.lua
+++ lua/kdtsk/settings/languages_frameworks/markdown.lua
@@ -2,6 +2,7 @@ return {
     {
         "MeanderingProgrammer/render-markdown.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+        cmd = { "RenderMarkdown" },
         ---@module 'render-markdown'
         ---@type render.md.UserConfig
         opts = {},
