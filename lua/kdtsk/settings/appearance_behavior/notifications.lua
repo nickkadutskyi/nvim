c14ac70a70a805353574ee80b94b086dab43a041
Nick Kadutskyi <nick@kadutskyi.com>
2025-06-25 09:06:21 -0700

    test

--- lua/kdtsk/settings/appearance_behavior/notifications.lua
+++ lua/kdtsk/settings/appearance_behavior/notifications.lua
@@ -2,8 +2,60 @@
 ---@type LazySpec
 return {
     {
+        "nickkadutskyi/snacks.nvim",
+        priority = 1000,
+        enabled = true,
+        lazy = false,
+        ---@type snacks.Config
+        opts = {
+            styles = {
+                notification = {
+                    wo = {
+                        wrap = true,
+                    },
+                },
+            },
+            notifier = {
+                timeout = 5000,
+                width = { min = 50, max = 50 },
+                margin = { top = 0, right = 1, bottom = 0, left = 3 },
+                style = function(buf, notif, ctx)
+                    vim.api.nvim_buf_set_lines(buf, 0, 0, false, { "", "" })
+                    vim.api.nvim_buf_set_lines(
+                        buf,
+                        1,
+                        -1,
+                        false,
+                        vim.tbl_map(function(str)
+                            return " " .. str
+                        end, vim.split(notif.msg, "\n"))
+                    )
+                    vim.api.nvim_buf_set_extmark(buf, ctx.ns, 0, 0, {
+                        virt_text = {
+                            { " " },
+                            { notif.icon, ctx.hl.icon },
+                            { " " },
+                            { notif.title or "", ctx.hl.title },
+                        },
+                        virt_text_win_col = 0,
+                        priority = 10,
+                    })
+                end,
+                top_down = false,
+                icons = {
+                    error = " ",
+                    warn = " ",
+                    info = " ",
+                    debug = " ",
+                    trace = " ",
+                },
+            },
+        },
+    },
+    {
         -- Notifications
         "rcarriga/nvim-notify",
+        enabled = false,
         -- pinned it due to bad update in next commit
         commit = "ab98fec",
         opts = {
