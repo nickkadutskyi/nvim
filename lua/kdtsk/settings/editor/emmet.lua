3ba24954f64ca78ee2bbb64900d280a586db8e75
Nick Kadutskyi <nick@kadutskyi.com>
2025-08-29 12:43:18 -0700

    emmet adjustment

--- lua/kdtsk/settings/editor/emmet.lua
+++ lua/kdtsk/settings/editor/emmet.lua
@@ -16,6 +16,7 @@ return {
                         "scss",
                         "vue",
                         "javascript",
+                        "blade",
                     },
                 },
             },
