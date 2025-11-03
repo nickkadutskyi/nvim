dc892d559a253bf0e5f4b284a2dbd4385a62a45c
Nick Kadutskyi <nick@kadutskyi.com>
2025-11-02 21:05:39 -0800

    otter: remove deprecated config

--- lua/kdtsk/settings/editor/lsp.lua
+++ lua/kdtsk/settings/editor/lsp.lua
@@ -474,7 +474,7 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         -- Due to not using it
-        enabled = false,
+        enabled = true,
         opts = {},
         config = function()
             local otter = require("otter")
@@ -499,7 +499,7 @@ return {
                     -- if set to true, the filetype of the otterbuffers will be set.
                     -- otherwise only the autocommand of lspconfig that attaches
                     -- the language server will be executed without setting the filetype
-                    set_filetype = false,
+                    -- set_filetype = false,
                     -- write <path>.otter.<embedded language extension> files
                     -- to disk on save of main buffer.
                     -- usefule for some linters that require actual files
