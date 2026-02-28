3ea81a53437ac12253674358daf54d6241af4de2
Nick Kadutskyi <nick@kadutskyi.com>
2026-02-28 13:06:07 -0800

    fix plugin order

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,8 +1,12 @@
 local spec_builder = require("ide.spec.builder")
 local pack = require("ide.pack")
 
-require("settings.behavior.notifications")
+-- In this file we define all the plugins with their `src` so we load it first
+-- to keep the order of how plugins are going to be loaded deterministic.
+-- This will help with dependendcies.
 require("settings.plugins")
+
+require("settings.behavior.notifications")
 
 --- Load plugins after all specs have been added and merged
 pack.load(spec_builder.get_specs())
